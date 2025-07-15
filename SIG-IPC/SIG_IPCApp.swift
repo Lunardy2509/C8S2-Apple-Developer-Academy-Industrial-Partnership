3fd862ae60549a6295c7f77922af93e3ce489e73
jonathancalvin <jonathancalvin16@gmail.com>
2025-07-15 16:49:51 +0700

    Initial Commit

---
+++ SIG-IPC/SIG_IPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIG_IPCApp.swift
+//  SIG-IPC
+//
+//  Created by jonathan calvin sutrisna on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SIG_IPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
